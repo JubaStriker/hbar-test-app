2ecb8c8885450e52b26f6f009df501e821e79cda
JubaStriker <jubastriker@gmail.com>
2023-05-02 19:42:54 +0600

    app rnning successfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+
 import Home from "./Home.tsx";
+
 function App() {
+
   return (
     <div>
       <Home></Home>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
     </div>
   );
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,8 @@
 import { AccountId, PrivateKey, Client } from '@hashgraph/sdk';
 import NavBar from './components/Navbar.tsx';
+import { sendHbar } from './services/hederaService.ts';
+import { useContext } from 'react';
+import { GlobalAppContext } from './contexts/GlobalAppContext.tsx';
 
 const Home = () => {
 
@@ -8,7 +11,7 @@ const Home = () => {
         throw new Error("Environment variables REACT_APP_MY_ACCOUNT_ID and REACT_APP_MY_PRIVATE_KEY must be present");
     }
 
-    console.log(process.env.REACT_APP_MY_ACCOUNT_ID)
+    const { metamaskAccountAddress } = useContext(GlobalAppContext);
 
     // create your client
     const myAccountId = AccountId.fromString(process.env.REACT_APP_MY_ACCOUNT_ID);
@@ -19,11 +22,32 @@ const Home = () => {
     client.setOperator(myAccountId, myPrivateKey);
 
     return (
-        <div className='py-4 bg-black flex justify-center relative'>
-            <h1 className='text-white my-auto text-center font-semibold text-2xl'>Building a dApp on Hedera</h1>
-            <div className='flex justify-end items-center absolute right-2'><NavBar /></div>
+        <>
+            <div className='py-4 bg-black flex justify-center relative'>
+                <h1 className='text-white my-auto text-center font-semibold text-2xl'>Building a dApp on Hedera</h1>
+                <div className='flex justify-end items-center absolute right-2'><NavBar /></div>
 
-        </div>
+            </div>
+
+            <div className='bg-slate-800 min-h-screen'>
+                <h1
+                    className="text-purple-600 text-center font-semibold pt-5"
+                >
+                    Let's build a dApp on Hedera
+                </h1>
+                <div className='flex justify-center mt-8 '>
+                    <button
+                        className="bg-violet-500 text-white px-4 rounded-xl py-2 "
+                        onClick={() => {
+                            sendHbar(client, myAccountId, AccountId.fromEvmAddress(0, 0, metamaskAccountAddress), 7, myPrivateKey)
+                        }}
+                    >
+                        Transfer HBAR to MetaMask Account
+                    </button>
+                </div>
+            </div>
+
+        </>
     );
 };
 
